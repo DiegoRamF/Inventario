1d881097b9a668b88637894ee149b856d874d885
Diego Ramirez <diegorf339@gmail.com>
2026-03-31 22:38:40 -0500

    Sidebar implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'inventario',
+    title: 'Inventario',
+    loadComponent: () => import('./inventario/inventario'),
+  },
+  {
+    path: '**',
+    redirectTo: 'inventario'
+  }
+];
---
+++ src/app/inventario/inventario.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inventario',
+  imports: [],
+  templateUrl: './inventario.html',
+})
+export default class Inventario { }
+
